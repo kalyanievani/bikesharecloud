5da44d164df322605d866db2a8c76b617cf7231e
Kalyani Evani <kalyani.evani@ushur.com>
2025-04-05 11:49:41 +0530

    added a comment

--- bikeshare_project/bikeshare_api/app/api.py
+++ bikeshare_project/bikeshare_api/app/api.py
@@ -19,7 +19,7 @@ from app.config import settings
 
 api_router = APIRouter()
 
-
+#Just a comment
 @api_router.get("/health", response_model=schemas.Health, status_code=200)
 def health() -> dict:
     """
